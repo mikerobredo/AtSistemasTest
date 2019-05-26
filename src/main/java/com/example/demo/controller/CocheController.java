352e20a4120ace185a8c7c73b80af5ad47ae921c
mikerobredo <elrobredo@gmail.com>
2019-05-26 02:08:46 +0200

    Corregido un error a nivel de perdida de datos local

--- src/main/java/com/example/demo/controller/CocheController.java
+++ src/main/java/com/example/demo/controller/CocheController.java
@@ -1,8 +1,7 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +9,22 @@ import com.example.demo.dto.CocheDto;
 
 
 
+
 @RestController
 @RequestMapping("/coche")
 public class CocheController {
-	private List<CocheDto> books = new ArrayList<>();
+	//private List<CocheDto> books = new ArrayList<>();
+	
+	@GetMapping("/{id}")
+	public CocheDto findOne(@PathVariable("id")Integer id)
+	{
+		CocheDto c1= new  CocheDto("2232","verde","opel","kadet");
+		
+		return c1;
+
+	}
+	
 	
 
 }
+
--- src/main/java/com/example/demo/model/Coche.java
+++ src/main/java/com/example/demo/model/Coche.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
@@ -12,6 +13,7 @@ import javax.persistence.OneToMany;
 import lombok.Data;
 
 @Data
+@Entity
 public class Coche {
 	@Id	
 	private String matricula;
